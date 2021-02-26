237e661fb7e50fbe494d958ce7c4f04bc8792b2f
thvitti <thvittix@gmail.com>
2021-02-26 16:04:22 -0300

    new transaction route created and transaction added to AccountController

--- lib/rocketpay_web/views/account_view.ex
+++ lib/rocketpay_web/views/account_view.ex
@@ -6,4 +6,14 @@ defmodule RocketpayWeb.AccountsView do
       account: %{id: account_id, balance: balance}
     }
   end
+
+  def render("transaction.json", %{transaction: %{to_account: to_account, from_account: from_account} }) do
+    %{
+      message: "Transaction done succefully",
+      transaction: %{ 
+        from_account: %{id: from_account.id, balance: from_account.balance},
+        to_account: %{id: to_account.id, balance: to_account.balance}
+      }
+    }
+  end
 end
