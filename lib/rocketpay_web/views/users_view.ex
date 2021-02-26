a705438eb6918b373607283d48f68913b4e91112
thvitti <thvittix@gmail.com>
2021-02-25 21:59:29 -0300

    added account info to RocketpayWeb.UsersView when insert a new user

--- lib/rocketpay_web/views/users_view.ex
+++ lib/rocketpay_web/views/users_view.ex
@@ -1,10 +1,10 @@
 defmodule RocketpayWeb.UsersView do
-    alias Rocketpay.User
+    alias Rocketpay.{Account,User}
 
-    def render("create.json", %{user: %User{ id: id, name: name, nickname: nickname }}) do
-        %{
-            message: "User created",
-            user: %{ id: id, name: name, nickname: nickname }
-        }
+    def render("create.json", %{user: %User{ id: id, name: name, nickname: nickname, account: %Account{id: account_id, balance: balance}}} ) do
+        account_data = %{id: account_id, balance: balance}
+        user_data = %{ id: id, name: name, nickname: nickname, account: account_data } 
+        
+        %{ message: "User created", user: user_data }
     end
 end
