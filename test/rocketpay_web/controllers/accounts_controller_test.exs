ad6ce3ef5cfae9ea7504e928985ce172911b2106
thvitti <thvittix@gmail.com>
2021-02-27 14:23:06 -0300

    test to AccountController.withdraw added

--- test/rocketpay_web/controllers/accounts_controller_test.exs
+++ test/rocketpay_web/controllers/accounts_controller_test.exs
@@ -23,8 +23,8 @@ defmodule RocketpayWeb.AccountsControllerTest do
         params = %{value: "3.0"}
 
         response = conn
-        |> post( Routes.accounts_path(conn, :deposit, account_id, params) )
-        |> json_response(:ok)
+          |> post( Routes.accounts_path(conn, :deposit, account_id, params) )
+          |> json_response(:ok)
 
         assert %{"account" => %{"balance" => "3.00", "id" => ^account_id}, "message" => "Ballance changed succefully"} = response
     end
@@ -39,5 +39,50 @@ defmodule RocketpayWeb.AccountsControllerTest do
       expected_response = %{"message" => "Invalid operation value!"}
       assert expected_response == response
     end
+  end
+
+  describe "withdraw/2" do
+    setup %{conn: conn} do
+      params = %{
+        name: "Marge Simpson",
+        nickname: "marge",
+        age: 38,
+        password: "saxphone",
+        email: "marge@thesimpsons.com"
+      }
+
+      {:ok, %User{ account: %Account{id: account_id}} } = Rocketpay.create_user(params)
+      conn = put_req_header(conn, "authorization", "Basic YmFuYW5hOm5hbmljYTEyMw==")
+      {:ok, conn: conn, account_id: account_id}
+    end
+
+    test "when got valid params and an account with balance, make the withdraw", %{conn: conn, account_id: account_id} do
+      params_deposit = %{"id" => account_id, "value" => "4.00"}
+      params_withdraw = %{value: "2.00"}
+      expected_balance = "2.00"
+
+      Rocketpay.deposit(params_deposit)
+
+      response = 
+        conn
+        |> post( Routes.accounts_path(conn, :withdraw, account_id, params_withdraw) ) 
+        |> json_response(:ok)
+
+      expected_response = %{"account" => %{"balance" => expected_balance, "id" => account_id}, "message" => "Ballance changed succefully"}
+      assert expected_response == response
+    end
+
+    test "when got invalid params, return an error", %{conn: conn, account_id: account_id} do
+      params = %{value: "invalid_value"}
+
+      response = 
+        conn
+        |> post( Routes.accounts_path(conn, :withdraw, account_id, params) )
+        |> json_response(400)
+
+      expected_response = %{"message" => "Invalid operation value!"}
+      assert expected_response == response
+    end
+
   end
 end
